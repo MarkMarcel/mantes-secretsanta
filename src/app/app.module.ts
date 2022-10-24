eabee1a2dd311a069d7fec467a6e37639388affe
Mark Marcel <freelancemarcel18@gmail.com>
2022-10-24 08:46:52 +0000

    styling changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import {MatDialogModule} from '@angular/material/dialog';
+import {MatDividerModule} from '@angular/material/divider';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
@@ -61,6 +62,7 @@ import { AdultComponent } from './widgets/adult/adult.component';
     MatButtonModule,
     MatCardModule,
     MatDialogModule,
+    MatDividerModule,
     MatFormFieldModule,
     MatGridListModule,
     MatIconModule,
@@ -76,7 +78,7 @@ import { AdultComponent } from './widgets/adult/adult.component';
     ReactiveFormsModule,
   ],
   providers: [
-    { provide: MAT_SNACK_BAR_DEFAULT_OPTIONS, useValue: { duration: 3000 } },
+    { provide: MAT_SNACK_BAR_DEFAULT_OPTIONS, useValue: { duration: 3000,verticalPosition:'top'} },
     { provide: MAT_RADIO_DEFAULT_OPTIONS, useValue: { color: 'primary' }, }
   ],
   bootstrap: [AppComponent]
--- src/app/widgets/child/child.component.ts
+++ src/app/widgets/child/child.component.ts
@@ -1,19 +1,43 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, EventEmitter, Input, OnDestroy, Output } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Subscription } from 'rxjs';
 import { ImageType } from 'src/app/image/image.service';
 import { Child } from 'src/models/child';
+import { ImageUploadMeta } from 'src/models/image-upload-meta';
+import { ImageUploadComponent } from '../image-upload/image-upload.component';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.scss']
 })
-export class ChildComponent implements OnInit {
-  @Input() child:Child|null = null
-  ImageType = ImageType
+export class ChildComponent implements AfterViewInit, OnDestroy {
+  @Input() child:Child|null = null 
+  pictureUrl: string = ''
+  @Output() url = new EventEmitter<string>();
 
-  constructor() { }
+  private _dialogRef: Subscription | null = null;
 
-  ngOnInit(): void {
+  constructor(
+    private _dialog: MatDialog
+  ) { }
+
+  ngAfterViewInit(): void {
+    this.pictureUrl = this.child!!.pictureUrl;
+  }
+
+  ngOnDestroy(): void {
+    this._dialogRef?.unsubscribe();
+  }
+
+  onUpdateImage() {
+    this._dialogRef?.unsubscribe();
+    this._dialogRef = this._dialog.open<ImageUploadComponent, ImageUploadMeta, string>(ImageUploadComponent,{data:{id:this.child!!.id,type:ImageType.CHILD}}).afterClosed().subscribe((url) => {
+      if (url) {
+        this.pictureUrl = url;
+        this.url.emit(url);
+      }
+    });
   }
 
 }
